c73f5d61039f351ddfc96e1ffa03c416cda904bc
andrebontempo <andre.lnx@gmail.com>
2022-03-04 09:00:37 -0300

    Decimo commit

---
+++ src/bontempo/com/main/ClassePrincipal.java
@@ -0,0 +1,13 @@
+package bontempo.com.main;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+	    //System.out.println(new Scanner(System.in).nextLine());
+		System.out.println("Primeira Classe em 2022");	
+	}
+
+}
